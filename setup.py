e0b27b6ae21e4284b858f15d48a14410fabca856
toolsknowledge <udaysicet@gmail.com>
2025-05-20 12:06:50 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'uday kiran challa',
+    author_email= 'udaysicet@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
